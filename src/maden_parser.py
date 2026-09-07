662440ea722f57c8b0ae3e178c4052f7909b9686
@maden <166928262+maden77@users.noreply.github.com>
2026-09-08 02:02:18 +0700

    Implement AST nodes and parser for new language

---
+++ src/maden_parser.py
@@ -0,0 +1,378 @@
+from maden_lexer import TokenType, Token
+
+class ASTNode:
+    pass
+
+# Jenis-jenis node AST
+class Program(ASTNode):
+    def __init__(self, pernyataan):
+        self.pernyataan = pernyataan
+
+class PernyataanFungsi(ASTNode):
+    def __init__(self, nama, parameter, tubuh, anotasi=None):
+        self.nama = nama
+        self.parameter = parameter
+        self.tubuh = tubuh
+        self.anotasi = anotasi or []
+
+class PernyataanKembali(ASTNode):
+    def __init__(self, nilai):
+        self.nilai = nilai
+
+class PernyataanCetak(ASTNode):
+    def __init__(self, ekspresi):
+        self.ekspresi = ekspresi
+
+class PernyataanJika(ASTNode):
+    def __init__(self, kondisi, tubuh, selain=None):
+        self.kondisi = kondisi
+        self.tubuh = tubuh
+        self.selain = selain
+
+class PernyataanUlangi(ASTNode):
+    def __init__(self, inisialisasi, kondisi, iterasi, tubuh):
+        self.inisialisasi = inisialisasi
+        self.kondisi = kondisi
+        self.iterasi = iterasi
+        self.tubuh = tubuh
+
+class PernyataanUntukSetiap(ASTNode):
+    def __init__(self, variabel, iterable, tubuh):
+        self.variabel = variabel
+        self.iterable = iterable
+        self.tubuh = tubuh
+
+class PernyataanUlangiSelama(ASTNode):
+    def __init__(self, kondisi, tubuh):
+        self.kondisi = kondisi
+        self.tubuh = tubuh
+
+class PernyataanImport(ASTNode):
+    def __init__(self, modul):
+        self.modul = modul
+
+class EkspresiVariabel(ASTNode):
+    def __init__(self, nama):
+        self.nama = nama
+
+class EkspresiAngka(ASTNode):
+    def __init__(self, nilai):
+        self.nilai = nilai
+
+class EkspresiTeks(ASTNode):
+    def __init__(self, nilai):
+        self.nilai = nilai
+
+class EkspresiBoolean(ASTNode):
+    def __init__(self, nilai):
+        self.nilai = nilai
+
+class EkspresiBinOp(ASTNode):
+    def __init__(self, kiri, operator, kanan):
+        self.kiri = kiri
+        self.operator = operator
+        self.kanan = kanan
+
+class EkspresiPanggilan(ASTNode):
+    def __init__(self, nama, argumen):
+        self.nama = nama
+        self.argumen = argumen
+
+class EkspresiPenugasan(ASTNode):
+    def __init__(self, nama, nilai, anotasi=None):
+        self.nama = nama
+        self.nilai = nilai
+        self.anotasi = anotasi
+
+class Parser:
+    def __init__(self, token):
+        self.token = token
+        self.pos = 0
+    
+    def parse(self) -> Program:
+        pernyataan = []
+        while self.peek().tipe != TokenType.EOF:
+            pernyataan.append(self.pernyataan())
+        return Program(pernyataan)
+    
+    def peek(self) -> Token:
+        return self.token[self.pos]
+    
+    def maju(self):
+        self.pos += 1
+    
+    def cocok(self, tipe):
+        if self.peek().tipe == tipe:
+            return self.maju()
+        return None
+    
+    def harap(self, tipe, pesan=None):
+        if self.peek().tipe == tipe:
+            return self.maju()
+        if pesan is None:
+            pesan = f"Diharapkan {tipe}, dapat {self.peek().tipe}"
+        raise SyntaxError(pesan)
+    
+    def pernyataan(self):
+        token = self.peek()
+        
+        # Anotasi (bisa di awal pernyataan)
+        anotasi = []
+        while token.tipe in [TokenType.INT8, TokenType.INT128, TokenType.ACCELERATOR, 
+                             TokenType.PRECISION, TokenType.PARALLEL]:
+            anotasi.append(token)
+            self.maju()
+            token = self.peek()
+        
+        # Fungsi
+        if token.tipe == TokenType.FUNGSI:
+            return self.pernyataan_fungsi(anotasi)
+        
+        # Kembali
+        if token.tipe == TokenType.KEMBALI:
+            self.maju()
+            if self.peek().tipe == TokenType.NEWLINE:
+                return PernyataanKembali(None)
+            nilai = self.ekspresi()
+            self.harap(TokenType.NEWLINE, "Diharapkan baris baru setelah return")
+            return PernyataanKembali(nilai)
+        
+        # Cetak
+        if token.tipe == TokenType.CETAK:
+            self.maju()
+            self.harap(TokenType.KURUNG_BUKA)
+            ekspresi = self.ekspresi()
+            self.harap(TokenType.KURUNG_TUTUP)
+            self.harap(TokenType.NEWLINE, "Diharapkan baris baru setelah cetak")
+            return PernyataanCetak(ekspresi)
+        
+        # Jika
+        if token.tipe == TokenType.JIKA:
+            self.maju()
+            kondisi = self.ekspresi()
+            self.harap(TokenType.KURUNG_KURUNG_BUKA)
+            tubuh = self.blok()
+            self.harap(TokenType.KURUNG_KURUNG_TUTUP)
+            
+            # Cek else if atau else
+            selain = None
+            if self.peek().tipe == TokenType.SELAIN:
+                self.maju()
+                if self.peek().tipe == TokenType.JIKA:
+                    # else if
+                    self.maju()
+                    kondisi_else = self.ekspresi()
+                    self.harap(TokenType.KURUNG_KURUNG_BUKA)
+                    tubuh_else = self.blok()
+                    self.harap(TokenType.KURUNG_KURUNG_TUTUP)
+                    selain = PernyataanJika(kondisi_else, tubuh_else, None)
+                else:
+                    self.harap(TokenType.KURUNG_KURUNG_BUKA)
+                    tubuh_else = self.blok()
+                    self.harap(TokenType.KURUNG_KURUNG_TUTUP)
+                    selain = tub
+                    selain = tubuh_else
+            
+            return PernyataanJika(kondisi, tubuh, selain)
+        
+        # Ulangi (for loop)
+        if token.tipe == TokenType.ULANGI:
+            self.maju()
+            self.harap(TokenType.KURUNG_BUKA)
+            inisialisasi = self.ekspresi_penugasan()
+            self.harap(TokenType.TITIK_KOMA)
+            kondisi = self.ekspresi()
+            self.harap(TokenType.TITIK_KOMA)
+            iterasi = self.ekspresi()
+            self.harap(TokenType.KURUNG_TUTUP)
+            self.harap(TokenType.KURUNG_KURUNG_BUKA)
+            tubuh = self.blok()
+            self.harap(TokenType.KURUNG_KURUNG_TUTUP)
+            return PernyataanUlangi(inisialisasi, kondisi, iterasi, tubuh)
+        
+        # Untuk setiap
+        if token.tipe == TokenType.UNTUK:
+            self.maju()
+            self.harap(TokenType.SETIAP)
+            variabel = self.harap(TokenType.IDENTIFIER)
+            self.harap(TokenType.DALAM)
+            iterable = self.ekspresi()
+            self.harap(TokenType.KURUNG_KURUNG_BUKA)
+            tubuh = self.blok()
+            self.harap(TokenType.KURUNG_KURUNG_TUTUP)
+            return PernyataanUntukSetiap(variabel.nilai, iterable, tubuh)
+        
+        # Ulangi selama (while)
+        if token.tipe == TokenType.ULANGI:
+            self.maju()
+            self.harap(TokenType.SELAMA)
+            kondisi = self.ekspresi()
+            self.harap(TokenType.KURUNG_KURUNG_BUKA)
+            tubuh = self.blok()
+            self.harap(TokenType.KURUNG_KURUNG_TUTUP)
+            return PernyataanUlangiSelama(kondisi, tubuh)
+        
+        # Import
+        if token.tipe == TokenType.IMPOR:
+            self.maju()
+            modul = self.harap(TokenType.IDENTIFIER).nilai
+            self.harap(TokenType.NEWLINE)
+            return PernyataanImport(modul)
+        
+        # Penugasan atau panggilan fungsi
+        if token.tipe == TokenType.IDENTIFIER:
+            nama = token.nilai
+            self.maju()
+            
+            # Panggilan fungsi
+            if self.peek().tipe == TokenType.KURUNG_BUKA:
+                self.maju()
+                argumen = []
+                if self.peek().tipe != TokenType.KURUNG_TUTUP:
+                    argumen.append(self.ekspresi())
+                    while self.peek().tipe == TokenType.KOMA:
+                        self.maju()
+                        argumen.append(self.ekspresi())
+                self.harap(TokenType.KURUNG_TUTUP)
+                self.harap(TokenType.NEWLINE)
+                return EkspresiPanggilan(nama, argumen)
+            
+            # Penugasan
+            if self.peek().tipe == TokenType.SAMA_DENGAN:
+                self.maju()
+                nilai = self.ekspresi()
+                self.harap(TokenType.NEWLINE)
+                return EkspresiPenugasan(nama, nilai, anotasi)
+        
+        # Baris baru kosong
+        if token.tipe == TokenType.NEWLINE:
+            self.maju()
+            return self.pernyataan()
+        
+        raise SyntaxError(f"Pernyataan tidak dikenal: {token}")
+    
+    def blok(self):
+        pernyataan = []
+        while self.peek().tipe != TokenType.KURUNG_KURUNG_TUTUP and self.peek().tipe != TokenType.EOF:
+            pernyataan.append(self.pernyataan())
+        return pernyataan
+    
+    def ekspresi(self):
+        return self.ekspresi_logika()
+    
+    def ekspresi_logika(self):
+        kiri = self.ekspresi_perbandingan()
+        
+        while self.peek().tipe in [TokenType.DAN, TokenType.ATAU]:
+            operator = self.peek()
+            self.maju()
+            kanan = self.ekspresi_perbandingan()
+            kiri = EkspresiBinOp(kiri, operator.nilai, kanan)
+        
+        return kiri
+    
+    def ekspresi_perbandingan(self):
+        kiri = self.ekspresi_penjumlahan()
+        
+        while self.peek().tipe in [TokenType.LEBIH_BESAR, TokenType.LEBIH_KECIL,
+                                    TokenType.LEBIH_BESAR_SAMA, TokenType.LEBIH_KECIL_SAMA,
+                                    TokenType.SAMA_DENGAN_DUA, TokenType.TIDAK_SAMA]:
+            operator = self.peek()
+            self.maju()
+            kanan = self.ekspresi_penjumlahan()
+            kiri = EkspresiBinOp(kiri, operator.nilai, kanan)
+        
+        return kiri
+    
+    def ekspresi_penjumlahan(self):
+        kiri = self.ekspresi_perkalian()
+        
+        while self.peek().tipe in [TokenType.TAMBAH, TokenType.KURANG]:
+            operator = self.peek()
+            self.maju()
+            kanan = self.ekspresi_perkalian()
+            kiri = EkspresiBinOp(kiri, operator.nilai, kanan)
+        
+        return kiri
+    
+    def ekspresi_perkalian(self):
+        kiri = self.ekspresi_unary()
+        
+        while self.peek().tipe in [TokenType.KALI, TokenType.BAGI, TokenType.MODULUS]:
+            operator = self.peek()
+            self.maju()
+            kanan = self.ekspresi_unary()
+            kiri = EkspresiBinOp(kiri, operator.nilai, kanan)
+        
+        return kiri
+    
+    def ekspresi_unary(self):
+        if self.peek().tipe == TokenType.KURANG:
+            self.maju()
+            operan = self.ekspresi_unary()
+            return EkspresiBinOp(EkspresiAngka(0), '-', operan)
+        if self.peek().tipe == TokenType.NOT:
+            self.maju()
+            operan = self.ekspresi_unary()
+            return EkspresiBinOp(EkspresiBoolean(False), 'not', operan)
+        return self.ekspresi_primary()
+    
+    def ekspresi_primary(self):
+        token = self.peek()
+        
+        if token.tipe == TokenType.ANGKA:
+            self.maju()
+            return EkspresiAngka(token.nilai)
+        
+        if token.tipe == TokenType.TEKS:
+            self.maju()
+            return EkspresiTeks(token.nilai)
+        
+        if token.tipe == TokenType.BENAR:
+            self.maju()
+            return EkspresiBoolean(True)
+        
+        if token.tipe == TokenType.SALAH:
+            self.maju()
+            return EkspresiBoolean(False)
+        
+        if token.tipe == TokenType.KOSONG:
+            self.maju()
+            return EkspresiBoolean(None)
+        
+        if token.tipe == TokenType.IDENTIFIER:
+            nama = token.nilai
+            self.maju()
+            
+            # Panggilan fungsi
+            if self.peek().tipe == TokenType.KURUNG_BUKA:
+                self.maju()
+                argumen = []
+                if self.peek().tipe != TokenType.KURUNG_TUTUP:
+                    argumen.append(self.ekspresi())
+                    while self.peek().tipe == TokenType.KOMA:
+                        self.maju()
+                        argumen.append(self.ekspresi())
+                self.harap(TokenType.KURUNG_TUTUP)
+                return EkspresiPanggilan(nama, argumen)
+            
+            return EkspresiVariabel(nama)
+        
+        if token.tipe == TokenType.KURUNG_BUKA:
+            self.maju()
+            ekspresi = self.ekspresi()
+            self.harap(TokenType.KURUNG_TUTUP)
+            return ekspresi
+        
+        raise SyntaxError(f"Ekspresi tidak dikenal: {token}")
+    
+    def ekspresi_penugasan(self):
+        if self.peek().tipe == TokenType.IDENTIFIER:
+            nama = self.peek().nilai
+            self.maju()
+            if self.peek().tipe == TokenType.SAMA_DENGAN:
+                self.maju()
+                nilai = self.ekspresi()
+                return EkspresiPenugasan(nama, nilai)
+            return EkspresiVariabel(nama)
+        return self.ekspresi()
