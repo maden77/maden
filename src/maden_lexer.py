55557db5c012bc1578845884d7a5682c24de58fe
ggo334 <ggo334538@gmail.com>
2026-09-18 14:19:57 +0700

    Perbaikan lexer: tambahkan keyword lowercase dan operator relasional yang hilang

--- src/maden_lexer.py
+++ src/maden_lexer.py
@@ -18,14 +18,17 @@ class TokenType:
     BENAR = "BENAR"
     SALAH = "SALAH"
     KOSONG = "KOSONG"
-    
+    DAN = "DAN"
+    ATAU = "ATAU"
+    NOT = "NOT"
+
     # Tipe data & anotasi
     INT8 = "INT8"
     INT128 = "INT128"
     ACCELERATOR = "ACCELERATOR"
     PRECISION = "PRECISION"
     PARALLEL = "PARALLEL"
-    
+
     # Simbol
     IDENTIFIER = "IDENTIFIER"
     ANGKA = "ANGKA"
@@ -51,9 +54,6 @@ class TokenType:
     LEBIH_KECIL_SAMA = "LEBIH_KECIL_SAMA"
     SAMA_DENGAN_DUA = "SAMA_DENGAN_DUA"
     TIDAK_SAMA = "TIDAK_SAMA"
-    DAN = "DAN"
-    ATAU = "ATAU"
-    NOT = "NOT"
     KOMENTAR = "KOMENTAR"
     NEWLINE = "NEWLINE"
     EOF = "EOF"
@@ -64,7 +64,7 @@ class Token:
         self.nilai = nilai
         self.baris = baris
         self.kolom = kolom
-    
+
     def __repr__(self):
         return f"Token({self.tipe}, {repr(self.nilai)}, baris={self.baris})"
 
@@ -75,14 +75,14 @@ class Lexer:
         self.baris = 1
         self.kolom = 1
         self.token = []
-        
+
         # Kata kunci
         self.keyword = {
             "fungsi": TokenType.FUNGSI,
             "kembali": TokenType.KEMBALI,
             "cetak": TokenType.CETAK,
             "jika": TokenType.JIKA,
-            "Selain": TokenType.SELAIN,
+            "selain": TokenType.SELAIN,
             "ulangi": TokenType.ULANGI,
             "untuk": TokenType.UNTUK,
             "setiap": TokenType.SETIAP,
@@ -92,8 +92,12 @@ class Lexer:
             "benar": TokenType.BENAR,
             "salah": TokenType.SALAH,
             "kosong": TokenType.KOSONG,
+            "dan": TokenType.DAN,
+            "atau": TokenType.ATAU,
+            "not": TokenType.NOT,
+            "tidak": TokenType.NOT,
         }
-        
+
         # Anotasi
         self.annotations = {
             "@int8": TokenType.INT8,
@@ -102,17 +106,17 @@ class Lexer:
             "@precision": TokenType.PRECISION,
             "@parallel": TokenType.PARALLEL,
         }
-    
+
     def tokenisasi(self) -> List[Token]:
         while self.pos < len(self.kode):
             char = self.kode[self.pos]
-            
+
             # Abaikan spasi
             if char == ' ' or char == '\t':
                 self.pos += 1
                 self.kolom += 1
                 continue
-            
+
             # Baris baru
             if char == '\n':
                 self.token.append(Token(TokenType.NEWLINE, '\n', self.baris, self.kolom))
@@ -120,30 +124,34 @@ class Lexer:
                 self.baris += 1
                 self.kolom = 1
                 continue
-            
+
             # Komentar
             if char == '/' and self.pos + 1 < len(self.kode) and self.kode[self.pos + 1] == '/':
                 self.pos += 2
                 while self.pos < len(self.kode) and self.kode[self.pos] != '\n':
                     self.pos += 1
                 continue
-            
+
             # Teks (string)
             if char == '"' or char == "'":
-                return self._token_teks(char)
-            
+                self._token_teks(char)
+                continue
+
             # Angka
             if char.isdigit() or (char == '-' and self.pos + 1 < len(self.kode) and self.kode[self.pos + 1].isdigit()):
-                return self._token_angka()
-            
+                self._token_angka()
+                continue
+
             # Identifier atau kata kunci
             if char.isalpha() or char == '_':
-                return self._token_identifier()
-            
+                self._token_identifier()
+                continue
+
             # Anotasi @
             if char == '@':
-                return self._token_anotasi()
-            
+                self._token_anotasi()
+                continue
+
             # Simbol satu karakter
             simbol_map = {
                 '(': TokenType.KURUNG_BUKA,
@@ -160,71 +168,73 @@ class Lexer:
                 '*': TokenType.KALI,
                 '/': TokenType.BAGI,
                 '%': TokenType.MODULUS,
+                '>': TokenType.LEBIH_BESAR,
+                '<': TokenType.LEBIH_KECIL,
             }
-            
+
             if char in simbol_map:
                 self.token.append(Token(simbol_map[char], char, self.baris, self.kolom))
                 self.pos += 1
                 self.kolom += 1
                 continue
-            
+
             # Simbol dua karakter
             if char == '=' and self.pos + 1 < len(self.kode) and self.kode[self.pos + 1] == '=':
                 self.token.append(Token(TokenType.SAMA_DENGAN_DUA, '==', self.baris, self.kolom))
                 self.pos += 2
                 self.kolom += 2
                 continue
-            
+
             if char == '!' and self.pos + 1 < len(self.kode) and self.kode[self.pos + 1] == '=':
                 self.token.append(Token(TokenType.TIDAK_SAMA, '!=', self.baris, self.kolom))
                 self.pos += 2
                 self.kolom += 2
                 continue
-            
+
             if char == '>' and self.pos + 1 < len(self.kode) and self.kode[self.pos + 1] == '=':
                 self.token.append(Token(TokenType.LEBIH_BESAR_SAMA, '>=', self.baris, self.kolom))
                 self.pos += 2
                 self.kolom += 2
                 continue
-            
+
             if char == '<' and self.pos + 1 < len(self.kode) and self.kode[self.pos + 1] == '=':
                 self.token.append(Token(TokenType.LEBIH_KECIL_SAMA, '<=', self.baris, self.kolom))
                 self.pos += 2
                 self.kolom += 2
                 continue
-            
+
             if char == '=':
                 self.token.append(Token(TokenType.SAMA_DENGAN, '=', self.baris, self.kolom))
                 self.pos += 1
                 self.kolom += 1
                 continue
-            
+
             # Operator logika
             if char == '&' and self.pos + 1 < len(self.kode) and self.kode[self.pos + 1] == '&':
                 self.token.append(Token(TokenType.DAN, '&&', self.baris, self.kolom))
                 self.pos += 2
                 self.kolom += 2
                 continue
-            
+
             if char == '|' and self.pos + 1 < len(self.kode) and self.kode[self.pos + 1] == '|':
                 self.token.append(Token(TokenType.ATAU, '||', self.baris, self.kolom))
                 self.pos += 2
                 self.kolom += 2
                 continue
-            
+
             # Error
             raise SyntaxError(f"Karakter tidak dikenal: '{char}' di baris {self.baris}, kolom {self.kolom}")
-        
+
         self.token.append(Token(TokenType.EOF, None, self.baris, self.kolom))
         return self.token
-    
+
     def _token_teks(self, tanda: str):
         self.pos += 1
         self.kolom += 1
         mulai_baris = self.baris
         mulai_kolom = self.kolom
         teks = ""
-        
+
         while self.pos < len(self.kode) and self.kode[self.pos] != tanda:
             if self.kode[self.pos] == '\n':
                 self.baris += 1
@@ -232,31 +242,31 @@ class Lexer:
             teks += self.kode[self.pos]
             self.pos += 1
             self.kolom += 1
-        
+
         if self.pos >= len(self.kode):
             raise SyntaxError(f"Teks tidak ditutup di baris {mulai_baris}")
-        
+
         self.pos += 1  # Lewati tanda tutup
         self.kolom += 1
         self.token.append(Token(TokenType.TEKS, teks, mulai_baris, mulai_kolom))
-    
+
     def _token_angka(self):
         mulai_baris = self.baris
         mulai_kolom = self.kolom
         angka = ""
-        
+
         # Tanda negatif
         if self.kode[self.pos] == '-':
             angka += '-'
             self.pos += 1
             self.kolom += 1
-        
+
         # Angka bulat
         while self.pos < len(self.kode) and self.kode[self.pos].isdigit():
             angka += self.kode[self.pos]
             self.pos += 1
             self.kolom += 1
-        
+
         # Pecahan
         if self.pos < len(self.kode) and self.kode[self.pos] == '.':
             angka += '.'
@@ -266,41 +276,41 @@ class Lexer:
                 angka += self.kode[self.pos]
                 self.pos += 1
                 self.kolom += 1
-        
+
         # Ubah ke float atau int
         if '.' in angka:
             nilai = float(angka)
         else:
             nilai = int(angka)
-        
+
         self.token.append(Token(TokenType.ANGKA, nilai, mulai_baris, mulai_kolom))
-    
+
     def _token_identifier(self):
         mulai_baris = self.baris
         mulai_kolom = self.kolom
         identitas = ""
-        
+
         while self.pos < len(self.kode) and (self.kode[self.pos].isalnum() or self.kode[self.pos] == '_'):
             identitas += self.kode[self.pos]
             self.pos += 1
             self.kolom += 1
-        
+
         # Cek apakah ini kata kunci
         tipe = self.keyword.get(identitas, TokenType.IDENTIFIER)
         self.token.append(Token(tipe, identitas, mulai_baris, mulai_kolom))
-    
+
     def _token_anotasi(self):
         mulai_baris = self.baris
         mulai_kolom = self.kolom
         anotasi = "@"
         self.pos += 1
         self.kolom += 1
-        
+
         while self.pos < len(self.kode) and (self.kode[self.pos].isalpha() or self.kode[self.pos] == '_'):
             anotasi += self.kode[self.pos]
             self.pos += 1
             self.kolom += 1
-        
+
         tipe = self.annotations.get(anotasi)
         if tipe:
             self.token.append(Token(tipe, anotasi, mulai_baris, mulai_kolom))
